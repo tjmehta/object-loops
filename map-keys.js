e13058d5378f4de1f1598f888d392fd0514de424
tjmehta <tejesh.mehta@gmail.com>
2015-12-02 20:31:33 -0800

    fix map-keys documentation

--- map-keys.js
+++ map-keys.js
@@ -6,12 +6,12 @@ var isObject = require('101/is-object');
 var forEach = require('./for-each');
 
 /**
- * Creates a new object with the results of calling a provided function on every value in the object.
+ * Creates a new object with the results of calling a provided function on every key in the object.
  * @function module:object-loops/map
- * @param {object} [obj] - object to map values, not accepted if being used directly on Object.prototype
- * @param {mapCallback} callback - function that produces the new value for the new, mapped object
+ * @param {object} [obj] - object to map keys, not accepted if being used directly on Object.prototype
+ * @param {mapKeysCallback} callback - function that produces the new key for the new mapped object
  * @param {*} [thisArg] - optional. context to bind to callback
- * @returns {object} newly created object with mapped values
+ * @returns {object} newly created object with mapped keys
  */
 module.exports = mapKeys;
 
@@ -31,10 +31,10 @@ function mapKeys (obj, callback, thisArg) {
   return mapped;
 }
 /**
- * This callback type is called `mapCallback` and is displayed as a global symbol.
- * @callback mapCallback
+ * This callback type is called `mapKeysCallback` and is displayed as a global symbol.
+ * @callback mapKeysCallback
  * @param {string} key - object key (used in current iteration)
  * @param {*} val - value for key
- * @param {object} obj - object which values are being iterated
- * @returns {*} mappedValue - value for key in the new, mapped object
+ * @param {object} obj - object which keys are being iterated
+ * @returns {*} mappedKey - value for key in the new, mapped object
  */
--- map.js
+++ map.js
@@ -9,7 +9,7 @@ var forEach = require('./for-each');
  * Creates a new object with the results of calling a provided function on every value in the object.
  * @function module:object-loops/map
  * @param {object} [obj] - object to map values, not accepted if being used directly on Object.prototype
- * @param {mapCallback} callback - function that produces the new value for the new, mapped object
+ * @param {mapCallback} callback - function that produces the new value for the new mapped object
  * @param {*} [thisArg] - optional. context to bind to callback
  * @returns {object} newly created object with mapped values
  */
